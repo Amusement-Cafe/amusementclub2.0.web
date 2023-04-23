cd501969956ad6645de7644ce4c694493ec462b0
madebynoxc <glebx95@gmail.com>
2023-04-23 16:18:17 -0400

    Change footer

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,24 +1,14 @@
-/**
-=========================================================
-* Material Dashboard 2 React - v2.1.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2022 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
 
 // prop-types is a library for typechecking of props
 import PropTypes from "prop-types";
 
 // @mui material components
 import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
 import Icon from "@mui/material/Icon";
+import Stack from "@mui/material/Stack";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
 
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox";
@@ -27,19 +17,41 @@ import MDTypography from "components/MDTypography";
 // Material Dashboard 2 React base styles
 import typography from "assets/theme/base/typography";
 
-function Footer({ company, links }) {
-  const { href, name } = company;
+const getStartedLinks = [
+  { href: "https://discord.com/api/oauth2/authorize?client_id=340988108222758934&permissions=0&scope=bot%20applications.commands", name: "Invite" },
+  { href: "https://docs.amusement.cafe/", name: "Documentation" },
+  { href: "https://discordz.gg/kqgAvdX", name: "Support Discord" },
+]
+
+const devLinks = [
+  { href: "https://github.com/Amusement-Cafe/amusementclub2.0", name: "Bot Source" },
+  { href: "https://github.com/Amusement-Cafe", name: "Amusement Cafe" },
+  { href: "https://github.com/madebynoxc/amusementclub2.0.web", name: "Website Source" },
+  { href: "https://github.com/madebynoxc/amusement-club", name: "Legacy Bot" },
+  { href: "https://ko-fi.com/amusement", name: "Donate" },
+]
+
+const usingLinks = [
+  { href: "https://github.com/OceanicJS/Oceanic", name: "Oceanic" },
+  { href: "https://nodejs.org/", name: "NodeJS" },
+  { href: "https://www.mongodb.com/", name: "MongoDB" },
+  { href: "https://nextjs.org/", name: "Next.JS" },
+  { href: "https://vercel.com/", name: "Vercel" },
+  { href: "https://material-ui.com/", name: "mui" },
+]
+
+function Footer() {
   const { size } = typography;
 
-  const renderLinks = () =>
+  const renderLinks = (links) =>
     links.map((link) => (
-      <MDBox key={link.name} component="li" px={2} lineHeight={1}>
+      <Stack>
         <Link href={link.href} target="_blank">
           <MDTypography variant="button" fontWeight="regular" color="text">
             {link.name}
           </MDTypography>
         </Link>
-      </MDBox>
+      </Stack>
     ));
 
   return (
@@ -51,62 +63,70 @@ function Footer({ company, links }) {
       alignItems="center"
       px={1.5}
     >
-      <MDBox
+      <Grid
         display="flex"
-        justifyContent="center"
-        alignItems="center"
+        width="100%"
+        justifyContent="space-between"
+        alignItems="top"
         flexWrap="wrap"
+        flexDirection={{ xs: "column", lg: "row" }}
         color="text"
         fontSize={size.sm}
         px={1.5}
       >
-        &copy; {new Date().getFullYear()}, made with
-        <MDBox fontSize={size.md} color="text" mb={-0.5} mx={0.25}>
-          <Icon color="inherit" fontSize="inherit">
-            favorite
-          </Icon>
-        </MDBox>
-        by
-        <Link href={href} target="_blank">
-          <MDTypography variant="button" fontWeight="medium">
-            &nbsp;{name}&nbsp;
-          </MDTypography>
-        </Link>
-        for a better web.
-      </MDBox>
-      <MDBox
-        component="ul"
-        sx={({ breakpoints }) => ({
-          display: "flex",
-          flexWrap: "wrap",
-          alignItems: "center",
-          justifyContent: "center",
-          listStyle: "none",
-          mt: 3,
-          mb: 0,
-          p: 0,
+        <Grid item>
+          <Stack>
+            <MDBox 
+              src="https://amusementclub.nyc3.cdn.digitaloceanspaces.com/web/discord_logo_2021.svg"
+              component="img"
+              alt="Dicord Logo"
+              mb={0.75}
+              sx={{ width: "120px" }}
+              />
+            
+            <MDBox 
+              src="https://amusementclub.nyc3.cdn.digitaloceanspaces.com/web/amusement-cafe-smalltext.png"
+              component="img"
+              alt="Amusement Cafe Logo"
+              mb={0.75}
+              sx={{ width: "120px" }}
+              />
 
-          [breakpoints.up("lg")]: {
-            mt: 0,
-          },
-        })}
-      >
-        {renderLinks()}
-      </MDBox>
+            <MDTypography variant="button" fontWeight="bold" color="text">
+              support@amusement.cafe
+            </MDTypography>
+            <MDTypography variant="button" fontWeight="bold" color="text">
+              Website 
+              <Link href="https://twitter.com/madebynoxc" target="_blank"> @madebynoxc</Link>
+            </MDTypography>
+            <MDTypography variant="button" fontWeight="bold" color="text">
+              Bort art by 
+              <Link href="https://twitter.com/NAMIORII" target="_blank"> @NAMIORII</Link>
+            </MDTypography>
+          </Stack>
+        </Grid>
+        <Grid item>
+          <MDTypography variant="h6">Get Started</MDTypography>
+          {renderLinks(getStartedLinks)}
+        </Grid>
+        <Grid item>
+          <MDTypography variant="h6">Links</MDTypography>
+          {renderLinks(devLinks)}
+        </Grid>
+        <Grid item>
+          <MDTypography variant="h6">Using</MDTypography>
+          {renderLinks(usingLinks)}
+        </Grid>
+        <Grid item>
+          <MDBox></MDBox>
+        </Grid>
+        <Grid item>
+          <MDBox></MDBox>
+        </Grid>
+      </Grid>
     </MDBox>
   );
 }
-
-// Setting default values for the props of Footer
-Footer.defaultProps = {
-  company: { href: "https://www.creative-tim.com/", name: "Creative Tim" },
-  links: [
-    { href: "https://www.creative-tim.com/", name: "Creative Tim" },
-    { href: "https://www.creative-tim.com/presentation", name: "About Us" },
-    { href: "https://www.creative-tim.com/blog", name: "Blog" },
-    { href: "https://www.creative-tim.com/license", name: "License" },
-  ],
-};
 
 // Typechecking props for the Footer
 Footer.propTypes = {
